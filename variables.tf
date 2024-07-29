62b6003445022f93035843f1d7cdff5795d695f1
conip <51626146+conip@users.noreply.github.com>
2024-07-29 15:29:35 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "network_domains" {
+  type = map(any)
+  default = null
+}
+
+variable "strict_mode" {
+  type = bool
+  default = false
+  description = "if set to true - requires policy definition both directions. if false it is enough to allow on just 1 net domain"
+}
