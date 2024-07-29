40e85b066b10bab20ac067f856e0bf78f0acec59
conip <51626146+conip@users.noreply.github.com>
2024-07-29 15:29:14 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+locals {
+  network_domains_set = toset(keys(var.network_domains))
+  create_domain = length(local.network_domains_set) > 0 ? true : false
+  create_policy = length(local.policy) > 0 ? true : false
+
+  # policy = var.strict_mode ? toset(flatten([
+  #   for domain, values in local.network_domains_set : [
+  #     for value in values : 
+  #     "${sort([value, domain])[0]}~~${sort([value,domain])[1]}" 
+  #     if try(contains(local.network_domains_set[value], domain), false)
+  #   ]
+  # ])) : toset(flatten([
+  #   for domain, values in local.network_domains_set : [
+  #     for value in values : 
+  #     "${sort([value, domain])[0]}~~${sort([value,domain])[1]}" 
+  #   ]
+  # ]))
+  policy = var.strict_mode ? toset(flatten([
+    for domain, values in var.network_domains : [
+      for value in values : 
+      "${sort([value, domain])[0]}~~${sort([value,domain])[1]}" 
+      if try(contains(var.network_domains[value], domain), false)
+    ]
+  ])) : toset(flatten([
+    for domain, values in var.network_domains : [
+      for value in values : 
+      "${sort([value, domain])[0]}~~${sort([value,domain])[1]}" 
+    ]
+  ]))
+}
+#------------------------------------------- resources -----------------------------------------
+resource "aviatrix_segmentation_network_domain" "segmentation_network_domain" {
+  for_each    = local.create_domain ? local.network_domains_set : toset([])
+  domain_name = each.key
+}
+
+resource "aviatrix_segmentation_network_domain_connection_policy" "domain_policy" {
+  for_each      = local.create_policy ? local.policy : toset([])
+  domain_name_1 = trimspace(split("~~", each.key)[0])
+  domain_name_2 = trimspace(split("~~", each.key)[1])
+  depends_on = [ aviatrix_segmentation_network_domain.segmentation_network_domain ]
+}
