ecf01394c03a6343916bb742d7f25c6de42ef802
conip <51626146+conip@users.noreply.github.com>
2024-08-05 16:22:55 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,17 @@
 variable "network_domains" {
-  type = map(any)
+  type    = map(any)
   default = null
 }
 
 variable "strict_mode" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "if set to true - requires policy definition both directions. if false it is enough to allow on just 1 net domain"
+}
+
+variable "associations" {
+  description = "A map with network domain associations"
+  type        = map(map(string))
+  default     = {}
+  nullable    = false
 }
